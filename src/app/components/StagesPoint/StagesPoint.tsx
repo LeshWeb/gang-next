305d6564b90f4cd73362cfbba28f260a56ea4a2e
LeshWeb <279123165a@gmail.com>
2024-09-23 23:47:19 +0400

    PointsAnimateTrue

--- src/app/components/StagesPoint/StagesPoint.tsx
+++ src/app/components/StagesPoint/StagesPoint.tsx
@@ -1,18 +1,40 @@
 import styles from './StagesPoint.module.css'
 import Circle from '@/app/public/svgs/circle.svg'
-import Indicate from '@/app/public/svgs/indicaters/indicate1.svg'
+import Indicate1 from '@/app/public/svgs/indicaters/indicate1.svg'
+import Indicate2 from '@/app/public/svgs/indicaters/indicate2.svg'
+import Indicate3 from '@/app/public/svgs/indicaters/indicate3.svg'
+import ArrowLeft from '@/app/public/svgs/indicaters/arrow-left.svg'
+import ArrowRight from '@/app/public/svgs/indicaters/arrow-right.svg'
 
 export function StagesPoint() {
   return (
-    <>
+    <div className={styles.stages}>
       <div className={styles.stages_card}>
         <div className={styles.stages_circle}>
-          <Indicate className={styles.indicate_svg} />
+          <Indicate1 className={styles.indicate_svg} />
           <Circle className={styles.circle_svg} />
           <p className={styles.circle_text}>01</p>
         </div>
         <p className={styles.circle_desc}>Отправь паспортные данные</p>
       </div>
-    </>
+      <ArrowLeft />
+      <div className={styles.stages_card}>
+        <div className={styles.stages_circle}>
+          <Indicate2 className={styles.indicate_svg2} />
+          <Circle className={styles.circle_svg} />
+          <p className={styles.circle_text}>02</p>
+        </div>
+        <p className={styles.circle_desc}>Доставка и установка компьютера</p>
+      </div>
+      <ArrowRight className={styles.arrow_right} />
+      <div className={styles.stages_card}>
+        <div className={styles.stages_circle}>
+          <Indicate3 className={styles.indicate_svg3} />
+          <Circle className={styles.circle_svg} />
+          <p className={styles.circle_text}>03</p>
+        </div>
+        <p className={styles.circle_desc}>Готово, наслаждайся игрой</p>
+      </div>
+    </div>
   )
 }
