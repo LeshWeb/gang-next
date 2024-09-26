b4e940da55c537c88ea8621f3b92d7c4d693968b
LeshWeb <279123165a@gmail.com>
2024-09-26 17:11:23 +0400

    MainGridAdaptive

--- src/app/components/01_Sections/01_Main/Main.tsx
+++ src/app/components/01_Sections/01_Main/Main.tsx
@@ -26,17 +26,18 @@ export function Main() {
   const sidebar = useRef<HTMLDivElement | null>(null)
   const [slide, setSlide] = useState<number>(1)
   return (
-    <>
-      <main className={styles.main}>
-        <div className={styles.view}>
-          {/* <Swiper
-            className={styles.mySwiper}
+    <main className="main">
+      <div className={styles.main}>
+        <div className={styles.swiper_view}>
+          <Swiper
+            className={styles.swiper}
             modules={[Navigation]}
             navigation
             ref={swiperRef}
           >
             <SwiperSlide>
               <Image
+                className={styles.swiper_image}
                 src={FirstImage}
                 alt="Main first page image"
                 width="1200"
@@ -45,6 +46,7 @@ export function Main() {
             </SwiperSlide>
             <SwiperSlide>
               <Image
+                className={styles.swiper_image}
                 src={SecondImage}
                 alt="Main first page image"
                 width="1200"
@@ -53,35 +55,32 @@ export function Main() {
             </SwiperSlide>
             <SwiperSlide>
               <Image
+                className={styles.swiper_image}
                 src={ThreeImage}
                 alt="Main first page image"
                 width="1200"
                 height="844"
               />
             </SwiperSlide>
-          </Swiper> */}
+          </Swiper>
         </div>
-        <div className={styles.main_body}>
-          <div className={styles.boolet_button}>
-            <div className={styles.main_bollet}>
-              <p className={styles.main_desc}>
-                Лучшие решения для командной игры
-                <br />и свежих новинок с доставкой на дом
-              </p>
-              <h1 className={styles.main_title}>
-                Арендуй ПК
-                <br />и играй дома
-              </h1>
-            </div>
-            <PrimaryBtn title="Арендовать" size="big" href="#" />
+        <div className={styles.main_bollet}>
+          <p className={styles.main_desc}>
+            Лучшие решения для командной игры
+            <br />и свежих новинок с доставкой на дом
+          </p>
+          <h1 className={styles.main_title}>
+            Арендуй ПК
+            <br />и играй дома
+          </h1>
+        </div>
+        <PrimaryBtn title="Арендовать" size="big" href="#" />
+        <div className={styles.main_slidebar}>
+          <p className={styles.slidebar_start}>0{slide}</p>
+          <div className={styles.main_case}>
+            <div className={styles.main_loading} ref={sidebar}></div>
           </div>
-          <div className={styles.main_slidebar}>
-            <p className={styles.slidebar_start}>0{slide}</p>
-            <div className={styles.main_case}>
-              <div className={styles.main_loading} ref={sidebar}></div>
-            </div>
-            <p className={styles.slidebar_end}>03</p>
-          </div>
+          <p className={styles.slidebar_end}>03</p>
         </div>
         <div className={styles.main_down}>
           <Under />
@@ -121,7 +120,7 @@ export function Main() {
             />
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   )
 }
--- src/app/components/Under/Under.tsx
+++ src/app/components/Under/Under.tsx
@@ -3,17 +3,17 @@ import styles from './Under.module.css'
 export function Under() {
   return (
     <div className={styles.main__under}>
-      <div className={styles.under__item}>
-        <p className={styles.under__text}>Все девайсы включены</p>
+      <div className={`${styles.under__text} ${styles.text_1}`}>
+        Все девайсы включены
       </div>
-      <div className={styles.under__item}>
-        <p className={styles.under__text}>Оформи за 10 минут</p>
+      <div className={`${styles.under__text} ${styles.text_2}`}>
+        Оформи за 10 минут
       </div>
-      <div className={styles.under__item}>
-        <p className={styles.under__text}>Бесплатная доставка</p>
+      <div className={`${styles.under__text} ${styles.text_3}`}>
+        Бесплатная доставка
       </div>
-      <div className={styles.under__item}>
-        <p className={styles.under__text}>Аренда без залога</p>
+      <div className={`${styles.under__text} ${styles.text_4}`}>
+        Аренда без залога
       </div>
     </div>
   )
