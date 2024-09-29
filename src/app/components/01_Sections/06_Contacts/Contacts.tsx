4adacaacbcfd0b77e68b64525172b410ab987388
LeshWeb <279123165a@gmail.com>
2024-09-29 23:39:25 +0400

    ContactsAdapted

--- src/app/components/01_Sections/06_Contacts/Contacts.tsx
+++ src/app/components/01_Sections/06_Contacts/Contacts.tsx
@@ -1,11 +1,18 @@
 import { SocialMediaIcons } from '../../SocialMediaIcons/SocialMediaIcons'
 import { Title } from '../../Title/Title'
 import styles from './Contacts.module.css'
+import Image from 'next/image'
+import ContactsImg from '@/app/public/contacts/contacts.png'
 
 export function Contacts() {
   return (
     <section className="section">
       <div className={styles.contacts_container}>
+        <Image
+          src={ContactsImg}
+          alt="Игровое стол с ПК"
+          className={styles.image}
+        />
         <Title description={'Свяжись для заказа'} title={'Наши контакты'} />
         <div className={styles.social_media}>
           <SocialMediaIcons size="big" />
--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -4,7 +4,7 @@ import { Stages } from '@/app/components/01_Sections/02_Stages/Stages'
 import { Cards } from '@/app/components/01_Sections/03_Cards/Cards'
 import { Devices } from '@/app/components/01_Sections/04_Devices/Devices'
 import { Comments } from '@/app/components/01_Sections/05_Comments/Comments'
-// import { Contacts } from '@/app/components/01_Sections/06_Contacts/Contacts'
+import { Contacts } from '@/app/components/01_Sections/06_Contacts/Contacts'
 
 export default function Page() {
   return (
@@ -14,7 +14,7 @@ export default function Page() {
       <Cards />
       <Devices />
       <Comments />
-      {/* <Contacts /> */}
+      <Contacts />
     </div>
   )
 }
