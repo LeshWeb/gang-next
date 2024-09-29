a056a06aa2e959b3167733133be0c2dc405c7960
LeshWeb <279123165a@gmail.com>
2024-09-29 22:39:06 +0400

    Comment`AdaptedSwiper+

--- src/app/components/01_Sections/05_Comments/Comments.tsx
+++ src/app/components/01_Sections/05_Comments/Comments.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Title } from '../../Title/Title'
 import { CommentCard } from '../../CommentCard/CommentCard'
 import styles from './Comments.module.css'
@@ -15,32 +16,68 @@ import {
   comment_three,
 } from '@/app/data/comment-data'
 
+import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+// import required modules
+import { Pagination } from 'swiper/modules'
+
+import './styles.css'
+
 export function Comments() {
   return (
     <section className="section">
       <Title title="Доволен сервисом?" description="ОСТАВЬ ОТЗЫВ" />
       <div className={styles.feedback_cards}>
-        <CommentCard {...comment_one}>
-          <Image
-            src={userOne}
-            alt="People comment`s"
-            className={styles.image}
-          />
-        </CommentCard>
-        <CommentCard {...comment_two}>
-          <Image
-            src={userTwo}
-            alt="People comment`s"
-            className={styles.image}
-          />
-        </CommentCard>
-        <CommentCard {...comment_three}>
-          <Image
-            src={userThree}
-            alt="People comment`s"
-            className={styles.image}
-          />
-        </CommentCard>
+        <Swiper
+          spaceBetween={40}
+          className={styles.comment_swiper}
+          modules={[Pagination]}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            951: {
+              slidesPerView: 2,
+            },
+            1480: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <CommentCard {...comment_one}>
+              <Image
+                src={userOne}
+                alt="People comment`s"
+                className={styles.image}
+              />
+            </CommentCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <CommentCard {...comment_two}>
+              <Image
+                src={userTwo}
+                alt="People comment`s"
+                className={styles.image}
+              />
+            </CommentCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <CommentCard {...comment_three}>
+              <Image
+                src={userThree}
+                alt="People comment`s"
+                className={styles.image}
+              />
+            </CommentCard>
+          </SwiperSlide>
+        </Swiper>
       </div>
       <div className={styles.feedback_cta}>
         <SecondaryBtn title="Оставить отзыв" href={'#'} />
