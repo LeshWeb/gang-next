d80365836bf8fb07993fb8497d97af0edb1cbd1c
Aleksey Cherkasov <279123165a@gmail.com>
2025-03-18 21:28:22 +0400

    GotovoTwo

--- src/app/sevices/telegram.service.ts
+++ src/app/sevices/telegram.service.ts
@@ -2,7 +2,9 @@
 import axios from 'axios'
 import { IForm } from '../(placing)/order/OrderForm/OrderForm'
 
-const URL_API = `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`
+const TOKEN = '7398419695:AAHyEwS8T_8wOFLMCmNoCmBbnUDi4-85b9U'
+const CHAT_ID = '-4785099777'
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
 
 export type basketCookies = {
   minimum?: string
@@ -14,7 +16,7 @@ export const sendMessage = async (data: IForm, basket: basketCookies) => {
   const text = `Имя: ${data.name}\nТелефон: ${data.phone}\nМинимальный ${basket.minimum}\nПродвинутый ${basket.medium}\nЭксклюзивный ${basket.maxiumum}`
 
   axios.post(URL_API, {
-    chat_id: process.env.CHAT_ID,
+    chat_id: CHAT_ID,
     text: text,
   })
 }
